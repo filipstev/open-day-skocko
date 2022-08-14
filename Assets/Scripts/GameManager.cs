23504f8aaa3161369db0d88d5f946b880d93746c
Filip Stevanovic <filipstevanovic176@gmail.com>
2022-08-14 15:43:33 +0200

    init commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour {
+
+	public Animator panelGameOverAnim;
+	public Text gameScore;
+	public Text menuScore;
+
+	public void GameOver()
+	{
+		GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().enabled = false;
+		panelGameOverAnim.SetTrigger("Open");
+		menuScore.text = gameScore.text;
+		gameScore.gameObject.SetActive(false);
+	}
+
+
+	public void PlayAgain()
+	{
+		UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+	}
+}
